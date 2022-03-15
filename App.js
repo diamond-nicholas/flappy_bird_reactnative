93aff3e096cdb02051c538253ad5aec18006cb32
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-16 00:33:33 +0100

    add bird component

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Bird from './Components/Bird';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world this my app runs on expo</Text>
+      <Bird />
     </View>
   );
 }
