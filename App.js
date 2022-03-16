24d1623e8f83eab8171643f6f6a72018336916ac
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-16 08:40:30 +0100

    add logic to make bird fall

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import Bird from './Components/Bird';
 
 export default function App() {
+  const screenWidth = Dimensions.get('screen').width;
+  const screenHeight = Dimensions.get('screen').height;
+  const birdLeft = screenWidth / 2;
+  const [birdBottom, setbirdBottom] = useState(screenHeight / 2);
+  const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth);
+  const gravity = 3;
+  let gameTimerId;
+  // console.log(screenWidth, screenHeight);
+
+  //start of bird falling
+  useEffect(() => {
+    if (birdBottom > 0) {
+      gameTimerId = setInterval(() => {
+        setbirdBottom((birdBottom) => birdBottom - gravity);
+      }, 30);
+
+      return () => {
+        clearInterval(gameTimerId);
+      };
+    }
+  }, [birdBottom]);
+
+  // console.log(birdBottom);
+
+  useEffect(() => {
+    if (obstaclesLeft > 0) {
+      setInterval(() => {
+        setObstaclesLeft((obstaclesLeft) => obstaclesLeft - 5);
+      }, 30);
+    }
+  }, []);
   return (
     <View style={styles.container}>
-      <Bird />
+      <Bird birdBottom={birdBottom} birdLeft={birdLeft} />
     </View>
   );
 }
